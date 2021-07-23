70a5029649195737dd2f2d23d4e0aac65cd9d308
Adam Jones <jbensus@gmail.com>
2021-07-22 21:52:05 -0500

    Uploading application folders

---
+++ config/connection.js
@@ -0,0 +1,32 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  const config = {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+  sequelize = new Sequelize(process.env.DATABASE_URL, config);
+} else {
+
+  sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        port: 3306,
+      }
+    );
+}
+
+module.exports = sequelize;
