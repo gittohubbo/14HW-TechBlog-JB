2b3320a20bd8f3356c53de400ab20081fbea4557
Adam Jones <jbensus@gmail.com>
2021-08-07 14:37:16 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,21 @@ require("dotenv").config();
 
 let sequelize;
 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
+/*
 if (process.env.DATABASE_URL) {
   const config = {
     dialectOptions: {
@@ -28,5 +43,5 @@ if (process.env.DATABASE_URL) {
       }
     );
 }
-
+*/
 module.exports = sequelize;
