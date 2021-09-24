211573e29d8e012366b609d3ff9e321a97fc27ad
Adam Jones <jbensus@gmail.com>
2021-09-23 22:35:54 -0500

    env file changed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
-const sequelize = require("../config/connection");
-const { User, Post } = require("../models");
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
 
-const userData = require("./userData.json");
-const postData = require("./postData.json");
+const userData = require('./userData.json');
+const postData = require('./postData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
